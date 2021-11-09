30d529cd74aa9ccc1fa6559b87401a0b226080ec
D Luke Nelson <david.luke.nelson@gmail.com>
2021-11-09 09:50:23 -0700

    Use docker-compose exec -t to avoid TTY
    This is needed for these scripts to work in github actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@
 SCRIPTS_DIR=`dirname "$0"`
 . $SCRIPTS_DIR/.compose_env.sh $1
 
-docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec $EXEC_TARGET coverage run -m pytest .
-docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec $EXEC_TARGET coverage report
+docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec -T $EXEC_TARGET coverage run -m pytest .
+docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec -T $EXEC_TARGET coverage report
