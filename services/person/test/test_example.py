fb2897316cfa666cda8aa429be642bedda3cce78
D Luke Nelson <david.luke.nelson@gmail.com>
2021-11-09 09:05:14 -0700

    fail a test to try out CI

--- services/person/test/test_example.py
+++ services/person/test/test_example.py
@@ -8,3 +8,9 @@ def test_hello_world():
     This makes sure that we can import code from the main service
     """
     assert routes.hello_world() == 'hello world'
+
+def failing_test():
+    """
+    This test is meant to fail so I can test CI
+    """
+    assert False
