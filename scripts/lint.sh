3bb8754029434f8f9adba553c264f12938d5b82b
D Luke Nelson <david.luke.nelson@gmail.com>
2021-11-09 09:37:39 -0700

    Use . instead of source so it is more generic

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPTS_DIR=`dirname "$0"`
-source $SCRIPTS_DIR/.compose_env.sh $1
+. $SCRIPTS_DIR/.compose_env.sh $1
 
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec $EXEC_TARGET pylint $SERVICE_NAME
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec $EXEC_TARGET pylint test
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPTS_DIR=`dirname "$0"`
-source $SCRIPTS_DIR/.compose_env.sh $1
+. $SCRIPTS_DIR/.compose_env.sh $1
 
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec $EXEC_TARGET coverage run -m pytest .
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec $EXEC_TARGET coverage report
