ab5ce5b27b22e67c128e3ee5ba4f93dfcb7ddc7a
D Luke Nelson <david.luke.nelson@gmail.com>
2021-11-10 07:09:08 -0700

    Use exit code from lint to pass/fail github actions

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,4 +3,7 @@ SCRIPTS_DIR=`dirname "$0"`
 . $SCRIPTS_DIR/.compose_env.sh $1
 
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec -T $EXEC_TARGET pylint $SERVICE_NAME
+lint_exit_code=$?
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec -T $EXEC_TARGET pylint test
+lint_exit_code=$(($lint_exit_code + $?))
+exit $lint_exit_code
