2384f5268a80e086a3dda58b81f97a474be2b320
D Luke Nelson <david.luke.nelson@gmail.com>
2021-11-10 06:15:58 -0700

    Use exit code from tests to pass/fail github action

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,6 @@ SCRIPTS_DIR=`dirname "$0"`
 . $SCRIPTS_DIR/.compose_env.sh $1
 
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec -T $EXEC_TARGET coverage run -m pytest .
+test_exit_code=$?
 docker-compose --file $SCRIPTS_DIR/docker-compose.yml exec -T $EXEC_TARGET coverage report
+exit $test_exit_code
