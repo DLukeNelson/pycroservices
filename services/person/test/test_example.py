73f56555aafdaa18a00e7d6a40741b14bad688d6
D Luke Nelson <david.luke.nelson@gmail.com>
2021-11-12 08:10:51 -0700

    Simple change to person

--- services/person/test/test_example.py
+++ services/person/test/test_example.py
@@ -7,4 +7,4 @@ def test_hello_world():
     """
     This makes sure that we can import code from the main service
     """
-    assert routes.hello_world() == 'hello world'
+    assert routes.hello_world() == 'hello world!'
